988ab98ac1478c44d3466462223a9ad7e0b6f157
lea-2024 <leadev2024@gmail.com>
2024-05-22 15:11:35 -0300

    Correcciones longitudes permitidas en email y password

--- js/login.js
+++ js/login.js
@@ -16,12 +16,16 @@ formLogin.addEventListener('submit', (event) => {
 const validateForm = (event) => {
   const email = document.querySelector('#email').value;
   const password = document.querySelector('#password').value;
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // expresión regular para validar email en formato correcto
+  // const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // expresión regular para validar email en formato correcto
+  const emailRegex = /^(([^<>()[\]\\.,;:\s@\“]+(\.[^<>()[\]\\.,;:\s@\“]+)*)|(\“.+\“))@((\[[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}])|(([a-zA-Z\-0–9]+\.)+[a-zA-Z\-0–9]{2,}))$/; // expresión regular para validar email en formato correcto
 
-  // Validar email, vacio, formato correcto
+  // Validar email, vacio, formato correcto y longitud máxima
   if (email.trim() === '') {
     event.preventDefault();
     messageError('El email es obligatorio', errorEmail);
+  } else if (email.length > 60) {
+    event.preventDefault();
+    messageError('El email no debe superar los 60 caracteres', errorEmail);
   } else if (!emailRegex.test(email)) {
     event.preventDefault();
     messageError('Ingrese un email válido', errorEmail);
@@ -34,9 +38,9 @@ const validateForm = (event) => {
   if (password === '') {
     event.preventDefault();
     messageError('La contraseña es obligatoria', errorPassword);
-  } else if (password.length < 8) {
+  } else if (password.length < 8 || password.length > 20) {
     event.preventDefault();
-    messageError('Ingrese un mínimo de 8 caracteres', errorPassword);
+    messageError('Ingrese una contraseña entre 8 y 20 caractares', errorPassword);
   } else if (password.includes(' ')) {
     event.preventDefault();
     messageError('La contraseña no debe contener espacios', errorPassword);
